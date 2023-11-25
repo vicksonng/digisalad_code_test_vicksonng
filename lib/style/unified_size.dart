7e1f3a1d0ed84f1824ad700d3682dec7f70b8cc2
Vickson Ng <vicksonncf@gmail.com>
2023-11-25 11:57:49 +0800

    [feat][styles]: init Style System

---
+++ lib/style/unified_size.dart
@@ -0,0 +1,5 @@
+const double xsSize = 4.0;
+const double smSize = 8.0;
+const double mdSize = 16.0;
+const double lgSize = 32.0;
+const double xlSize = 64.0;
---
+++ lib/style/unified_styles.dart
@@ -0,0 +1,7 @@
+import 'package:digisalad_code_test_vicksonng/style/unified_size.dart';
+import 'package:flutter/material.dart';
+
+EdgeInsets get unifiedPagePadding => const EdgeInsets.symmetric(
+      horizontal: lgSize,
+      vertical: mdSize,
+    );
