1be3e523fbdd9437f6b730a8a09d60c7839ab12f
Vickson Ng <vicksonncf@gmail.com>
2023-11-25 16:19:53 +0800

    [feat][styles]: Updated unified page padding

--- lib/style/unified_styles.dart
+++ lib/style/unified_styles.dart
@@ -1,7 +1,6 @@
 import 'package:digisalad_code_test_vicksonng/style/unified_size.dart';
 import 'package:flutter/material.dart';
 
-EdgeInsets get unifiedPagePadding => const EdgeInsets.symmetric(
-      horizontal: lgSize,
-      vertical: mdSize,
-    );
+const EdgeInsets unifiedPagePadding = EdgeInsets.symmetric(
+  horizontal: lgSize,
+);
