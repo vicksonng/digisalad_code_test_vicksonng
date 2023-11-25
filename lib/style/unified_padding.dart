c07520c1fa5601bd63a77c6bf9c779a2c2c7f059
Vickson Ng <vicksonncf@gmail.com>
2023-11-25 18:26:05 +0800

    [update][style]: replaced unified styles with unified padding

---
+++ lib/style/unified_padding.dart
@@ -0,0 +1,11 @@
+import 'package:digisalad_code_test_vicksonng/style/unified_size.dart';
+import 'package:flutter/material.dart';
+
+class UnifiedPadding {
+  static const EdgeInsets horizontalLg = EdgeInsets.symmetric(
+    horizontal: lgSize,
+  );
+  static const EdgeInsets horizontalMd = EdgeInsets.symmetric(
+    horizontal: mdSize,
+  );
+}
--- lib/style/unified_styles.dart
+++
@@ -1,6 +0,0 @@
-import 'package:digisalad_code_test_vicksonng/style/unified_size.dart';
-import 'package:flutter/material.dart';
-
-const EdgeInsets unifiedPagePadding = EdgeInsets.symmetric(
-  horizontal: lgSize,
-);
