7fc8187af4e212984db15c6338bf22f4b0b27ba7
Vickson Ng <vicksonncf@gmail.com>
2023-11-25 20:52:01 +0800

    [update][style]: added color theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:digisalad_code_test_vicksonng/injections.dart';
 import 'package:digisalad_code_test_vicksonng/routes/routes.dart';
 import 'package:digisalad_code_test_vicksonng/features/music_list/view/music_list_page.dart';
 import 'package:digisalad_code_test_vicksonng/features/music_list/binding/music_list_page_binding.dart';
+import 'package:digisalad_code_test_vicksonng/style/unified_color.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -24,6 +25,13 @@ Widget _getApp() {
   return GetMaterialApp(
     getPages: Routes.getPages,
     initialBinding: MusicListPageBinding(),
+    theme: ThemeData.from(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: const Color(
+          UnifiedColor.themeColor,
+        ),
+      ),
+    ),
     home: const MusicListPage(),
   );
 }
---
+++ lib/style/unified_color.dart
@@ -0,0 +1,3 @@
+class UnifiedColor {
+  static const themeColor = 0xFF25c5d1;
+}
