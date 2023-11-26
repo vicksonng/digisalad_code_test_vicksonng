840f78ce96dfc955f7ff94e0e6133dad1fa3568f
Vickson Ng <vicksonncf@gmail.com>
2023-11-26 14:06:56 +0800

    [fix][gap]: removed gap extension & added unifiedGap

---
+++ lib/style/unified_gap.dart
@@ -0,0 +1,10 @@
+import 'package:digisalad_code_test_vicksonng/style/unified_size.dart';
+import 'package:gap/gap.dart';
+
+class UnifiedGap {
+  static Gap get xs => const Gap(xsSize);
+  static Gap get sm => const Gap(smSize);
+  static Gap get md => const Gap(mdSize);
+  static Gap get lg => const Gap(lgSize);
+  static Gap get xl => const Gap(xlSize);
+}
