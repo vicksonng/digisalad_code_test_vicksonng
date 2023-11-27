8db2ae8f406fe0f2cf8e1064c026d7e86d00b96c
Vickson Ng <vicksonncf@gmail.com>
2023-11-28 02:13:32 +0800

    [update][common]: updated cached image widget

--- lib/widgets/cached_image.dart
+++ lib/widgets/cached_image.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:digisalad_code_test_vicksonng/widgets/shimmer_wrapper.dart';
 import 'package:flutter/material.dart';
 
 class CachedImage extends StatelessWidget {
@@ -27,9 +28,29 @@ class CachedImage extends StatelessWidget {
       height: height,
       fit: fit,
       placeholder: (context, url) =>
-          placeholder ?? const CircularProgressIndicator(),
+          placeholder ?? _shimmerPlaceholder(context),
       errorWidget: (context, url, error) =>
-          errorWidget ?? const Icon(Icons.error),
+          errorWidget ?? _defaultErrorWidget(context),
+    );
+  }
+
+  Widget _shimmerPlaceholder(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return ShimmerWrapper(
+          child: Container(
+            height: constraints.maxHeight,
+            width: constraints.maxWidth,
+            color: Theme.of(context).splashColor,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _defaultErrorWidget(BuildContext context) {
+    return const Icon(
+      Icons.error,
     );
   }
 }
